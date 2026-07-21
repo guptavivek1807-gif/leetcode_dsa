ae4661a7c4e7acce6043184600cbb3fda64002e2
guptavivek1807-gif <guptavivek1807@gmail.com>
2026-07-21 08:38:58 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (99.87%) - LeetSync

---
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int n=s.size();
+        int hashlen=256;
+        int hash[hashlen];
+        for(int i=0;i<hashlen;++i){
+            hash[i]=-1;
+        }
+        int l=0;
+        int r=0;
+    int    maxlen=0;
+        while(r<n){
+            if(hash[s[r]]!=-1){
+        if(hash[s[r]]>=l){
+            l=hash[s[r]]+1;
+        }
+            }
+            int len=r-l+1;
+            maxlen=max(len,maxlen);
+            hash[s[r]]=r;
+            r++;
+        }
+        return maxlen;
+    }
+};
