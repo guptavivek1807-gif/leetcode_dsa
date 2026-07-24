f0a4c46c913f72118537f557eaf5e2372d9e124c
guptavivek1807-gif <guptavivek1807@gmail.com>
2026-07-24 08:14:32 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (43.63%) - LeetSync

---
+++ 812-rotate-string/rotate-string.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if (s.length() != goal.length()) {
+            return false;
+        }
+        // Try all possible rotations of 's'
+        for (int i = 0; i < s.length(); i++) {
+            string rotated = s.substr(i) + s.substr(0, i);  
+            if (rotated == goal) {
+                return true;
+            }
+        }
+        return false;
+        
+    }
+};
