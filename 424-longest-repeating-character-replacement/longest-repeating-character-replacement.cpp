19b29f6a1c76e071e384ea6fb9c83d6150d6135c
guptavivek1807-gif <guptavivek1807@gmail.com>
2026-08-08 08:57:29 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (42.18%) - LeetSync

---
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        vector<int>freq(26,0);
+        int left=0;
+        int right=0;
+        int maxcount=0;
+        int maxlen=0;
+        while(right<s.size()){
+            freq[s[right]-'A']++;
+            maxcount=max(maxcount,freq[s[right]-'A']);
+            while((right-left+1)-maxcount>k){
+                freq[s[left]-'A']--;
+                left++;
+            }
+            maxlen=max(maxlen,right-left+1);
+            right++;
+        }
+        return maxlen;
+    }
+};
