13f02b68d56ff7393a5a33ce2d0e584a184c8ee5
guptavivek1807-gif <guptavivek1807@gmail.com>
2026-08-22 11:45:03 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (75.05%) - LeetSync

---
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+        int minopen=0;
+       int  maxopen=0;
+        for(char c:s){
+            if(c=='('){
+                minopen++;
+                maxopen++;
+            }
+            else if(c==')'){
+                minopen--;
+                maxopen--;
+            }
+            else{
+                minopen--;
+                maxopen++;
+            }
+            if(maxopen<0) return false;
+            minopen=max(minopen,0);
+        }
+        return minopen==0;
+        
+    }
+};
