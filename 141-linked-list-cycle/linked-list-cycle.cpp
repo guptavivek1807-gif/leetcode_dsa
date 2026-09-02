07c11b11ab69b25a52db515693b8a14777353bdb
guptavivek1807-gif <guptavivek1807@gmail.com>
2026-09-02 08:37:37 +0530

    Time: 8 ms (81.70%) | Memory: 12 MB (22.46%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -8,26 +8,26 @@
  */
 class Solution {
 public:
+
+//tortoise method
     bool hasCycle(ListNode *head) {
         ListNode *slow = head;
         ListNode *fast = head;
 
-        // Step 2: Traverse the linked list with
-        // the slow and fast pointers
+        
         while (fast != nullptr && fast->next != nullptr) {
-            // Move slow one step
+            
             slow = slow->next;
-            // Move fast two steps
+           
             fast = fast->next->next;
 
-            // Check if slow and fast pointers meet
+            
             if (slow == fast) {
-                return true;  // Loop detected
+                return true;  
             }
         }
 
-        // If fast reaches the end of the list,
-        // there is no loop
+      
         return false;
         
     }
